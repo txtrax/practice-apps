1d3e671e5acd46d51bb3bcad664b8dfe6174e3cd
Theresa Tran <theresa.tran216@gmail.com>
2022-06-29 16:50:45 -0700

    implemented back end get route

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,6 +1,53 @@
 const mongoose = require("mongoose");
 
 // 1. Use mongoose to establish a connection to MongoDB
+mongoose.connect('mongodb://localhost/glossary');
 // 2. Set up any schema and models needed by the app
+let wordSchema = new mongoose.Schema({
+  word: {type: String, unique: true, required: true},
+  definition: String,
+  italicized: Boolean,
+  private: Boolean
+});
 // 3. Export the models
+let Word = mongoose.model('Word', wordSchema);
+
+// let dummyData = {
+//   word: 'Aaron',
+//   description: 'lord of pans',
+//   italicized: false,
+//   private: false
+// }
+
+let create = (obj) => {
+  console.log('inside create');
+
+  let newEntry = new Word({
+    word: obj.word,
+    definition: obj.description,
+    italicized: obj.italicized,
+    private: obj.private
+  })
+
+  newEntry.save()
+    .catch(err => {
+      console.log('error saving entries')
+    })
+}
+
+let read = () => {
+  return Word.find()
+}
+
+// let update = () => {
+
+// }
+
+// let delete = () => {
+// }
+
 // 4. Import the models into any modules that need them
+module.exports.create = create;
+module.exports.read = read;
+// module.exports.update = update;
+// module.exports.delete = delete;
--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,19 +1,52 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const db = require("./db.js");
 
 const app = express();
 
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.json());
 
-/**** 
- * 
- * 
- * Other routes here....
- *
- * 
- */
+//CRUD
+app.post('/words', (req, res) => {
+  console.log('made it in post', req.body)
+
+  // this does not complete?
+  db.create(req.body)
+    .then(results => {
+      console.log('success in server post')
+      res.end();
+    })
+    .catch(err => {
+      console.log('error in server post');
+      res.end();
+    })
+})
+
+app.get('/words', (req, res) => {
+  //this works
+  db.read()
+    .then(results => {
+      console.log('line 24', results);
+      res.end();
+    })
+    .catch(err => {
+      console.log('error retrieving from db')
+      res.send(500);
+    });
+})
+
+app.put('/words', (req, res) => {
+
+  res.end();
+})
+
+app.delete('/words', (req, res) => {
+
+  res.end();
+})
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
